4af9c29f02a3b2baa592a0849813c98f19a8e143
nominalrune <59037577+nominalrune@users.noreply.github.com>
2021-06-15 07:27:43 +0900

    Update m01_f.ts

--- m01_f.ts
+++ m01_f.ts
@@ -7,7 +7,7 @@ type BasicPrimitive=boolean|number|string;
  * - `getStore(key)`
  *   - gets `value` corresponds to the givven key.
  * - `setStore(key, value)`
- *   - sets `key` and``
+ *   - sets `key` and `valllu`
  */
 function useStore() {
 	const store: {[key:string]:BasicPrimitive} = {};
