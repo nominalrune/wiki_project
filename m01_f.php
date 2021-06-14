a87dce97ae3270c6cc03e0fae7161f73e18b2f10
nominalrune <59037577+nominalrune@users.noreply.github.com>
2021-06-15 07:36:32 +0900

    Update m01_f.php

--- m01_f.php
+++ m01_f.php
@@ -1,7 +1,17 @@
 <?php
 declare(strict_types=1);
 
-
+/**
+ * Stores key-value variables.
+ * 
+ * When called, this returns `getStore()` and `setStore()`:
+ * - `getStore(key)`
+ *   - gets `value` corresponds to the given key.
+ * - `setStore(key, value)`
+ *   - sets `key` and `value`, this overwrites the value when it's already set. Then returns a single `{key:value}` set of onject.
+ * 
+ * Notice each `key` must be `string` and `value` be scalar (that is, boolean, number, string).
+ */
 function use_store(array $initial=[])
 {
     /**@var (bool|int|float|string)[] $store */
