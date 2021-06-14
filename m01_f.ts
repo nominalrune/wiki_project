0116729653f599221e68f61133ffef432b3c1c18
nominalrune <59037577+nominalrune@users.noreply.github.com>
2021-06-15 07:26:12 +0900

    Update m01_f.ts

--- m01_f.ts
+++ m01_f.ts
@@ -5,7 +5,7 @@ type BasicPrimitive=boolean|number|string;
  * each `key` must be `string` and `value` be scalar (boolean, number, string).<br/><br/>
  * When called, this returns `getStore()` and `setStore()`:
  * - `getStore(key:string)`
- *   - gets `value`
+ *   - gets `value` 
  * 
  */
 function useStore() {
