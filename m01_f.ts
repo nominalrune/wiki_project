b8b6d635b21295e18fc1515ec8bcb03eae3a5ede
nominalrune <59037577+nominalrune@users.noreply.github.com>
2021-06-15 07:24:33 +0900

    Update m01_f.ts

--- m01_f.ts
+++ m01_f.ts
@@ -2,7 +2,7 @@ type BasicPrimitive=boolean|number|string;
 
 /**
  * stores key-value variables.<br/>
- * each `key` must be `string` and `value` be 
+ * each `key` must be `string` and `value` be scar
  * When called, returns `getStore()` and `setStore()`:
  * - `getStore(key:string)`
  *   - gets `value`
